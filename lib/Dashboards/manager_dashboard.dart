4087423d50a178ff44ac9c179505533e955880a9
MrSquid0 <gonzalo0@correo.ugr.es>
2024-04-21 20:51:56 +0200

    New screen of the manager included in the project

---
+++ lib/Dashboards/manager_dashboard.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import '../Login/login_screen.dart';
+import 'project_detail_screen.dart';
+import 'package:intl_phone_number_input/intl_phone_number_input.dart';
+import 'package:libphonenumber/libphonenumber.dart';
